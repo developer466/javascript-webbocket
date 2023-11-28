e8590552ded07abe05f45dbdb1426f87b5f8ad44
developer466 <152277662+developer466@users.noreply.github.com>
2023-11-28 11:59:43 +0000

    text file added

---
+++ 01-basics/text.js
@@ -0,0 +1,6 @@
+console.log("satya babu");
+/*1-js is high levle scripting language used to develop a web & mobile application efficiently
+2-we use js in client side as well as server side programming language
+3-js was developed to write logic/functionality to our code to perform every where
+4-js was developed in 1997 and the 1st version was ES1 and later on it was ES2....
+5-now we used ES6 version after 2015. ES6-Echma scripting 6(vanila js)
